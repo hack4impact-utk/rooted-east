2e04f3d5c7dff3a23fea527317ab2f8f311f1ac9
Randy Lin <rlin8@vols.utk.edu>
2024-05-01 18:18:45 -0400

    comment clarification

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,12 +1,13 @@
 import { createEvent } from '@/server/actions/Event';
 import { zCreateEventRequest } from '@/types/dataModel/event';
-// import { userAuth } from '@/utils/auth'; dont have
+// import { userAuth } from '@/utils/auth';
 import CMError, { CMErrorResponse, CMErrorType } from '@/utils/cmerror';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
-    // await userAuth(); we don't have user AUTH currently
+    // await userAuth();
+    // BUT WE NEED A USERAUTH to make it so managers / admins are the ones allowed to make events
 
     const body = await req.json();
 
