54326d0f47892d31bbb01aae86596e7eb3c440bd
efisk15 <128869985+efisk15@users.noreply.github.com>
2024-08-23 18:10:39 -0400

    Updated action.

--- src/server/actions/EventVolunteer.ts
+++ src/server/actions/EventVolunteer.ts
@@ -1,32 +1,25 @@
-import { CreateEventVolunteerRequest } from '@/types/dataModel/eventVolunteer';
+import { CheckInVolunteerRequest } from '@/types/dataModel/eventVolunteer';
 import dbConnect from '@/utils/db-connect';
 import CMError, { CMErrorType } from '@/utils/cmerror';
 import EventVolunteerSchema from '@/server/models/EventVolunteer';
 import { mongo } from 'mongoose';
 
 // Check if volunteer has already been checked in by seeing if the checkin time field has been set.
-export async function checkInVolunteer(
-  createEventVolunteerRequest: CreateEventVolunteerRequest
-): Promise<string | null> {
+export async function checkInVolunteerRequest(
+  checkInVolunteerRequest: CheckInVolunteerRequest
+): Promise<void> {
   try {
     await dbConnect();
 
-    // Check if volunteer has been signed in
-    if (await EventVolunteerSchema.exists(createEventVolunteerRequest)) {
-      console.log('Volunteer already signed in.');
-      return null;
-    }
-    const res = await EventVolunteerSchema.create(createEventVolunteerRequest);
+    const res = await EventVolunteerSchema.findByIdAndUpdate(
+      checkInVolunteerRequest.eventVolunteerId,
+      { checkInTime: Date.now() }
+    );
     // TODO for #58 handle a duplicate entry fail case here (duplicate event+volunteer ID combination)
     if (!res) {
       throw new Error('Volunteer could not be signed in.');
     }
-    console.log(
-      createEventVolunteerRequest.volunteer.firstName,
-      ' has been signed in.'
-    );
-
-    return res._id.toString();
+    console.log('Volunteer signed in.');
   } catch (error) {
     if (
       error instanceof mongo.MongoError ||
--- src/types/dataModel/eventVolunteer.ts
+++ src/types/dataModel/eventVolunteer.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import zVolunteer from './volunteer';
 import zEvent from './event';
+import EventVolunteer from '@/server/models/EventVolunteer';
 
 const zEventVolunteer = z.object({
   volunteer: zVolunteer,
@@ -9,10 +10,20 @@ const zEventVolunteer = z.object({
   checkOutTime: z.date().optional(),
 });
 
-export const zCreateEventVolunteerRequest = zEventVolunteer.extend({});
+export const zCreateEventVolunteerRequest = zEventVolunteer.extend({
+  volunteer: zVolunteer,
+  event: zEvent,
+});
+
+export const zCheckInVolunteerRequest = z.object({
+  eventVolunteerId: z.string(),
+  checkInTime: z.date(),
+});
 
 export interface EventVolunteer extends z.infer<typeof zEventVolunteer> {}
 export interface CreateEventVolunteerRequest
   extends z.infer<typeof zCreateEventVolunteerRequest> {}
+export interface CheckInVolunteerRequest
+  extends z.infer<typeof zCheckInVolunteerRequest> {}
 
 export default zEventVolunteer;
