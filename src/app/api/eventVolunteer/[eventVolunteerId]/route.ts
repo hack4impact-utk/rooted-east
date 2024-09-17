d4e8d8e7d190cecb3cfd59f4f2ce0eef299c4593
mylonjones <mylonndj1234@gmail.com>
2024-09-16 20:49:09 -0400

    moved contents of routes.ts to route.ts

--- src/app/api/eventVolunteer/[eventVolunteerId]/route.ts
+++ src/app/api/eventVolunteer/[eventVolunteerId]/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import CMError, { CMErrorResponse, CMErrorType } from '@/utils/cmerror';
 import { zObjectId } from '@/types/dataModel/base';
 import { deleteEventVolunteer } from '@/server/actions/EventVolunteer';
+import { zCheckInVolunteerRequest } from '@/types/dataModel/eventVolunteer';
+import { checkInVolunteerAction } from '@/server/actions/EventVolunteer';
 
 export async function DELETE(
   _request: NextRequest,
@@ -20,5 +22,34 @@ export async function DELETE(
     return new NextResponse(undefined, { status: 204 });
   } catch (error) {
     return CMErrorResponse(error);
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { eventVolunteerId: string } }
+) {
+  try {
+    const idValidationResult = zObjectId.safeParse(params.eventVolunteerId);
+    if (!idValidationResult.success) {
+      return new CMError(
+        CMErrorType.BadValue,
+        'EventVolunteer Id'
+      ).toNextResponse();
+    }
+
+    const data = await req.json();
+    const validationResult = zCheckInVolunteerRequest.safeParse(data);
+    if (!validationResult.success) {
+      return new CMError(
+        CMErrorType.BadValue,
+        'EventVolunteer'
+      ).toNextResponse();
+    }
+    await checkInVolunteerAction(params);
+
+    return new NextResponse(undefined, { status: 204 });
+  } catch (e) {
+    return CMErrorResponse(e);
   }
 }
