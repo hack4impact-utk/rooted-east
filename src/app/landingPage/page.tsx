db94204d08ec6ab78861a32f540623e73215dc54
mylonjones <mylonndj1234@gmail.com>
2024-10-13 15:05:14 -0400

    removed log

--- src/app/landingPage/page.tsx
+++ src/app/landingPage/page.tsx
@@ -4,7 +4,6 @@ import { getVolunteerEvents } from '@/server/actions/Event';
 
 export default async function LandingPage() {
   const events = await getVolunteerEvents('66d1374dc497b7361aa51451');
-  console.log(events[0]._id.toString());
   return (
     <div>
       <h1>This is the Landing Page</h1>
