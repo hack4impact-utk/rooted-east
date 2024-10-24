f43d4975633c95cde6ec742a3a7fd7bd574b3617
Randy Lin <rlin8@vols.utk.edu>
2024-10-23 20:44:52 -0400

    button added

--- src/app/buttonTest/page.tsx
+++ src/app/buttonTest/page.tsx
@@ -2,24 +2,15 @@ import LPHelpButton from '@/components/LandingPageHelpButton';
 import AddEventButton from '@/components/AddEventButton';
 import EventList from '@/components/EventList';
 import { getVolunteerEvents } from '@/server/actions/Event';
-import { getManagerVolunteers } from '@/server/actions/Volunteer';
 
 export default async function LandingPage() {
   const events = await getVolunteerEvents('670c85e7c68ff08582eb070b');
-  let volunteers = [];
-
-  try {
-    volunteers = await getManagerVolunteers();
-  } catch (error) {
-    console.error('Error fetching volunteers:', error);
-    return <div>Error fetching volunteers</div>;
-  }
 
   return (
     <div>
       <h1>This is the testing</h1>
       <LPHelpButton />
-      <AddEventButton managers={volunteers} />
+      <AddEventButton />
       <EventList events={events} />
     </div>
   );
