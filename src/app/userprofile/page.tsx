fb81326119b9bda8554c8d553914abcce2fc37da
mylonjones <mylonndj1234@gmail.com>
2024-11-10 09:27:03 -0500

    fixed non plain object warning

--- src/app/userprofile/page.tsx
+++ src/app/userprofile/page.tsx
@@ -3,9 +3,10 @@ import { Volunteer } from '@/types/dataModel/volunteer';
 import { getVolunteer } from '@/server/actions/Volunteer';
 import UserProfilePage from '@/components/UserProfilePage';
 export default async function UserProfile() {
-  const person: Volunteer | null = await getVolunteer(
-    '670c85e7c68ff08582eb070b'
-  );
+  let person: Volunteer | null = await getVolunteer('670c85e7c68ff08582eb070b');
+
+  person = JSON.parse(JSON.stringify(person));
+
   if (person) {
     return (
       <Box>
