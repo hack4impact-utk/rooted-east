b77b991c4c7658265a03fb750a17dd1cf633ed0a
mylonjones <mylonndj1234@gmail.com>
2024-11-15 17:44:27 -0500

    very basic auth

---
+++ src/services/authService.ts
@@ -0,0 +1,8 @@
+// import GoogleProvider from "next-auth/providers/google";
+
+// providers: [
+//   GoogleProvider({
+//     clientId: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//   })
+// ]
