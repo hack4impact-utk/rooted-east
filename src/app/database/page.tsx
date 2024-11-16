6466955580e6640eb3a00cd562170b7b7b8d326e
efisk15 <128869985+efisk15@users.noreply.github.com>
2024-11-15 22:39:15 -0500

    Added copy email and phone number buttons.

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -2,13 +2,22 @@ import DatabaseVolunteersList from '@/components/DatabaseVolunteersList';
 import NavBar from '@/components/NavBar';
 import { getAllVolunteers } from '@/server/actions/Volunteer';
 import CSVButton from '@/components/CSVButton';
+import CopyPhoneNumbersButton from '@/components/CopyPhoneNumbersButton';
+import CopyEmailsButton from '@/components/CopyEmailsButton';
+import { Box } from '@mui/material';
 
 export default async function Home() {
   const vols = await getAllVolunteers();
+  const newVols = JSON.parse(JSON.stringify(vols));
+
   return (
     <div>
       <NavBar />
-      <CSVButton vols={vols}></CSVButton>
+      <Box margin={'1rem'}>
+        <CSVButton vols={newVols}></CSVButton>
+        <CopyPhoneNumbersButton vols={newVols}></CopyPhoneNumbersButton>
+        <CopyEmailsButton vols={newVols}></CopyEmailsButton>
+      </Box>
       <DatabaseVolunteersList vols={JSON.parse(JSON.stringify(vols))} />
     </div>
   );
