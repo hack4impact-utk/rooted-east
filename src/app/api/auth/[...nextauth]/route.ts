0be3a1ff783ab2aeb4d15af82ca8b78e47c15c71
mylonjones <mylonndj1234@gmail.com>
2025-01-31 11:48:10 -0500

    unimportant commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,39 @@
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 import NextAuth from 'next-auth';
 import type { AuthOptions } from 'next-auth';
 
 export const authOptions: AuthOptions = {
   providers: [
+    CredentialsProvider({
+      name: 'Email & Password',
+      credentials: {
+        username: { label: 'Email', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = {
+          id: '1',
+          name: 'J Smith',
+          email: 'mylonndj1234@gmail.com',
+        };
+
+        console.log(credentials);
+        console.log(req);
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
