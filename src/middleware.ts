4f70af246cf1ce216e6bfcd70ed81aece05b75ff
Maggie <mgkelley01@gmail.com>
2025-02-08 13:32:46 -0500

    middleware changes for andrew

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,14 @@
 export { default } from 'next-auth/middleware';
+import { NextResponse, NextRequest } from 'next/server';
+import { getCurrentUser } from './utils/getCurrentUser';
+
+export async function middleware(request: NextRequest) {
+  const user = await getCurrentUser();
+  if (!user?.profileFinished) {
+    return NextResponse.redirect(new URL('/userprofile', request.url));
+  }
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ['/events', '/database', '/manageEvent/:path*', '/dashboard/:path*'],
