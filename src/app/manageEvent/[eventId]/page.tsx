3496f2c9262b9fce0bfa93a0899da4c255fb8c75
asmith7939 <jinxed@Korra>
2025-02-10 19:03:02 -0500

    changed the styling for manage event page

--- src/app/manageEvent/[eventId]/page.tsx
+++ src/app/manageEvent/[eventId]/page.tsx
@@ -11,7 +11,7 @@ import DeleteEventButton from '@/components/DeleteEventButton';
 import CopyPhoneNumbersButton from '@/components/CopyPhoneNumbersButton';
 import CopyEmailsButton from '@/components/CopyEmailsButton';
 import VolunteerSearchBar from '@/components/VolunteerSearchBar';
-import '@/styles.css';
+import '@/app/global.styles.css';
 import { VolunteerEntity } from '@/types/dataModel/volunteer';
 
 export default async function ManageEvent({
@@ -46,97 +46,57 @@ export default async function ManageEvent({
       <NavBar />
 
       {/* Title and Date Section */}
-      <Box sx={{ marginLeft: '1rem', marginBottom: '1rem' }}>
-        <Box
-          sx={{
-            bgcolor: '#324033',
-            padding: '1rem',
-            marginTop: '1rem',
-            marginBottom: '.5rem',
-            borderRadius: '8px',
-            width: 'fit-content',
-          }}
-        >
-          <Typography variant="h4" sx={{ color: '#D5C7BC' }}>
-            {event?.title}
+      <Box className="manage-event-parent">
+        <Box className="manage-event-title">
+          <Typography variant="h4">{event?.title}</Typography>
+        </Box>
+        <Box className="manage-event-boxes">
+          <Typography variant="subtitle1" className="manage-event-text">
+            {event?.day.toLocaleDateString('en-US', {
+              weekday: 'long',
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}{' '}
+            at{' '}
+            {event?.startTime.toLocaleTimeString([], {
+              hour: 'numeric',
+              minute: '2-digit',
+            })}{' '}
+            -{' '}
+            {event?.endTime.toLocaleTimeString([], {
+              hour: 'numeric',
+              minute: '2-digit',
+            })}
           </Typography>
         </Box>
-        <Typography
-          variant="subtitle1"
-          sx={{ color: '#666666', marginLeft: '0.5rem' }}
-        >
-          {event?.day.toLocaleDateString('en-US', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-          })}{' '}
-          at{' '}
-          {event?.startTime.toLocaleTimeString([], {
-            hour: 'numeric',
-            minute: '2-digit',
-          })}{' '}
-          -{' '}
-          {event?.endTime.toLocaleTimeString([], {
-            hour: 'numeric',
-            minute: '2-digit',
-          })}
-        </Typography>
       </Box>
-
-      {/* Buttons and Search Bar Section */}
-      <Box
-        sx={{
-          display: 'flex',
-          gap: '12rem',
-          alignItems: 'center',
-          marginLeft: '1rem',
-          marginRight: '1rem',
-          marginTop: '1rem',
-          marginBottom: '1rem',
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            gap: '2rem',
-            '& > *, & > * > button': {
-              width: '9.5rem !important',
-              minWidth: '9.5rem !important',
-            },
-          }}
-        >
-          <EditEventButton
-            event={JSON.parse(JSON.stringify(event))}
-            managers={formattedManagers}
-          />
-          <DeleteEventButton eventId={params.eventId} />
-          <CopyPhoneNumbersButton
-            vols={JSON.parse(JSON.stringify(filteredVols))}
-          />
-          <CopyEmailsButton vols={JSON.parse(JSON.stringify(filteredVols))} />
+      <Box className="manage-event-parent">
+        {/* Buttons and Search Bar Section */}
+        <Box className="manage-event-boxes">
+          <Box className="manage-event-buttons">
+            <EditEventButton
+              event={JSON.parse(JSON.stringify(event))}
+              managers={formattedManagers}
+            />
+            <DeleteEventButton eventId={params.eventId} />
+            <CopyPhoneNumbersButton
+              vols={JSON.parse(JSON.stringify(filteredVols))}
+            />
+            <CopyEmailsButton vols={JSON.parse(JSON.stringify(filteredVols))} />
+          </Box>
         </Box>
-
-        <Box sx={{ width: '400px' }}>
+      </Box>
+      <Box className="manage-event-parent">
+        <Box className="manage-event-search">
           <VolunteerSearchBar eventId={params.eventId} />
         </Box>
       </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginLeft: '1rem',
-        }}
-      >
-        <Typography
-          variant="subtitle1"
-          sx={{ color: '#666666', textAlign: 'center' }}
-        >
+      <Box className="manage-event-parent">
+        <Typography variant="subtitle1" className="manage-event-text">
           Volunteers Signed Up: {event?.volsSignUp}/{event?.volsNeeded}
         </Typography>
       </Box>
-
       {/* Volunteers List */}
       <RegisteredVolsList
         vols={JSON.parse(JSON.stringify(filteredVols))}
