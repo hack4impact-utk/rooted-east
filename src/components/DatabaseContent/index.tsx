dcaf358287b834a48f4f3b5e9c15cda0849de436
mylonjones <mylonndj1234@gmail.com>
2025-03-19 16:55:44 -0400

    refactored the database page

---
+++ src/components/DatabaseContent/index.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { VolunteerEntity } from '@/types/dataModel/volunteer';
+import React, { useState } from 'react';
+import DatabaseVolunteersList from '@/components/DatabaseVolunteersList';
+import '@/app/global.styles.css';
+import CSVButton from '@/components/CSVButton';
+import CopyPhoneNumbersButton from '@/components/CopyPhoneNumbersButton';
+import CopyEmailsButton from '@/components/CopyEmailsButton';
+import VolunteerSearchBar from '@/components/VolunteerSearchBar';
+import { Box } from '@mui/material';
+import AddVolunteerButton from '@/components/AddVolunteerButton';
+import UserProfilePage from '@/components/UserProfilePage';
+
+interface DatabaseContentProps {
+  vols: VolunteerEntity[];
+  currentUser: VolunteerEntity | null;
+}
+
+export default function DatabaseContent(props: DatabaseContentProps) {
+  const { vols, currentUser } = props;
+
+  const [currPerson, setCurPerson] = useState<VolunteerEntity | null>(null);
+
+  const handleOpenUserProfile = (person: VolunteerEntity) => {
+    if (person._id == currPerson?._id) {
+      setCurPerson(null);
+    } else {
+      setCurPerson(person);
+    }
+  };
+
+  return (
+    <div>
+      <div className="database-buttons-and-list">
+        <Box className="database-parent">
+          <Box className="database-buttons">
+            <CSVButton vols={vols} />
+            <CopyPhoneNumbersButton vols={vols} />
+            <CopyEmailsButton vols={vols} />
+            <AddVolunteerButton />
+          </Box>
+          <Box className="database-parent">
+            <Box className="database-search">
+              <VolunteerSearchBar basePath="/database" />
+            </Box>
+          </Box>
+        </Box>
+        <DatabaseVolunteersList
+          vols={vols}
+          handleOpenUserProfile={handleOpenUserProfile}
+        />
+      </div>
+      {currPerson && currentUser && (
+        <Box className="more-info-button">
+          <UserProfilePage currentUser={currentUser} person={currPerson} />
+        </Box>
+      )}
+    </div>
+  );
+}
--- src/components/DatabaseVolunteersList/index.tsx
+++ src/components/DatabaseVolunteersList/index.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import { Box, List, ListItem } from '@mui/material';
 import { VolunteerEntity } from '@/types/dataModel/volunteer';
-// import MoreParticipantInfo from '../MoreParticipantInfo';
-import { getCurrentUser } from '@/utils/getCurrentUser';
-import CMError, { CMErrorType } from '@/utils/cmerror';
+import MoreParticipantInfo from '../MoreParticipantInfo';
 
 interface VolObjectList {
   vols: VolunteerEntity[];
+  handleOpenUserProfile(person: VolunteerEntity | null): void;
 }
 
-export default async function DatabaseVolunteersList(props: VolObjectList) {
-  const currentUser = await getCurrentUser();
-  //check for undefined user
-  if (!currentUser) {
-    throw new CMError(CMErrorType.NoSuchKey, 'Volunteer');
-  }
+export default function DatabaseVolunteersList(props: VolObjectList) {
+  const { vols, handleOpenUserProfile } = props;
 
   return (
     <Box className="database-container">
       <List>
-        {props.vols.map(async (vol: VolunteerEntity, index) => {
+        {vols.map((vol: VolunteerEntity, index) => {
           return (
             <ListItem key={index} className="database-list-item">
               <Box className="database-names">
@@ -27,10 +22,10 @@ export default async function DatabaseVolunteersList(props: VolObjectList) {
               </Box>
               <Box className="database-emails">{vol.email}</Box>
               <Box className="database-more-info">
-                {/* <MoreParticipantInfo
-                  currentUser={JSON.parse(JSON.stringify(currentUser))}
+                <MoreParticipantInfo
+                  handleOpenUserProfile={handleOpenUserProfile}
                   person={vol}
-                /> */}
+                />
               </Box>
             </ListItem>
           );
