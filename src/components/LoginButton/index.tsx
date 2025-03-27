3440f0653a942e1a6a362f202ac62bb34c2f1847
magsncheese <mgkelley01@gmail.com>
2025-03-27 12:45:19 -0400

    changed the sign up and welcome message

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -10,7 +10,8 @@ export default function LoginButton() {
     // return null; // This displays when user *is* logged in
     return (
       <>
-        {/* <hr className="welcome-hr" /> */}
+        <h4 className="welcome-h4">Welcome!</h4>
+        <h4 className="welcome-emojis">🍓🌿🥕</h4>
         <h5 className="welcome-h5">
           This is the Rooted East Participant Management System.
         </h5>
@@ -33,7 +34,7 @@ export default function LoginButton() {
 
   return (
     <>
-      <h4 className="welcome-h4"> Welcome! </h4>
+      <h4 className="welcome-h4"> Sign In! </h4>
       <h4 className="welcome-emojis">🍓🌿🥕</h4>
       <hr className="welcome-hr" />
       <h5 className="welcome-h5">
